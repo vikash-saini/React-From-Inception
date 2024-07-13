f21e1a49e386ef2a46ee65a9c3ed5b42d4d9dad5
Vikash Saini <vikash.saini@ibm.com>
2024-07-13 18:14:06 +0530

    react inception episode1

---
+++ App.js
@@ -0,0 +1,21 @@
+
+  /* <div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h1>I am h2 tag</h1>
+    </div>
+</div> */
+
+
+let parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
